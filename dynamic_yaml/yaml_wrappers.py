4a2bb45681f47d498a8a1409570e073f9be22da4
Liam Childs <liam.h.childs@gmail.com>
2020-02-23 14:09:22 +0100

    update missed python 2 to 3 code

--- dynamic_yaml/yaml_wrappers.py
+++ dynamic_yaml/yaml_wrappers.py
@@ -4,23 +4,23 @@ from collections import Sequence, Mapping
 class YamlDict(dict):
 
     def __init__(self, *args, **kwargs):
-        super(YamlDict, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self._root = self
 
     def __repr__(self):
         tmp = self._root
         self._root = None
-        res = super(YamlDict, self).__repr__()
+        res = super().__repr__()
         self._root = tmp
         return res
 
     def __getattr__(self, key):
         if key in self:
             return self[key]
-        return super(YamlDict, self).__getattribute__(key)
+        return super().__getattribute__(key)
 
     def __getitem__(self, key):
-        v = super(YamlDict, self).__getitem__(key)
+        v = super().__getitem__(key)
         if self._root is not None and isinstance(v, str):
             v = v.format(**self._root)
         return v
@@ -33,7 +33,7 @@ class YamlDict(dict):
             pass
         elif isinstance(value, Sequence) and not isinstance(value, YamlList):
             value = YamlList(value)
-        super(YamlDict, self).__setitem__(key, value)
+        super().__setitem__(key, value)
 
     def set_as_root(self, root=None):
         if root is None:
@@ -48,11 +48,11 @@ class YamlList(list):
     ROOT_NAME = 'root'
 
     def __init__(self, *args, **kwargs):
-        super(YamlList, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.__root = {YamlList.ROOT_NAME: self}
 
     def __getitem__(self, key):
-        v = super(YamlList, self).__getitem__(key)
+        v = super().__getitem__(key)
         if isinstance(v, str):
             v = v.format(**self.__root)
         return v
@@ -62,7 +62,7 @@ class YamlList(list):
             value = YamlDict(value)
         elif isinstance(value, Sequence) and not isinstance(value, YamlList):
             value = YamlList(value)
-        super(YamlList, self).__setitem__(key, value)
+        super().__setitem__(key, value)
 
     def set_as_root(self, root=None):
         if root is None:
