47c962a662b040dc61b537b4473d6c9c791c97cc
Liam Childs <liam.h.childs@gmail.com>
2020-02-23 13:46:47 +0100

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,33 @@
 from setuptools import setup
+from subprocess import Popen, PIPE
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+def get_root_path():
+    return path.dirname(path.realpath(__file__))
+
+
+def get_version():
+    return Popen(['git', 'describe', '--tags', '--dirty'], stdout=PIPE, cwd=get_root_path()).communicate()[0]
+
+
+def read_file(filename):
+    return open(path.join(get_root_path(), filename)).read()
+
 
 setup(
     name='dynamic-yaml',
-    version='1.1.4',
+    version=get_version(),
     description='Enables self referential yaml entries',
-    long_description=long_description,
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
     url='https://github.com/childsish/dynamic-yaml',
     author='Liam Childs',
     author_email='liam.h.childs@gmail.com',
     license='MIT',
     packages=['dynamic_yaml'],
+    python_requires='>=3.0',
     install_requires=['pyyaml'],
     extras_require={
         'dev': [
