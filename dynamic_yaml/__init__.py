922fb0f422d2b2041f4c918163d42c12b0f146c6
Liam Childs <liam.h.childs@gmail.com>
2020-02-29 07:54:24 +0100

    add add_wrappers

--- dynamic_yaml/__init__.py
+++ dynamic_yaml/__init__.py
@@ -1,27 +1,31 @@
 import yaml
 
-
-def add_constructors(loader):
-    from .yaml_wrappers import YamlDict, YamlList
-
-    def _construct_sequence(loader, node):
-        return YamlList(loader.construct_object(child) for child in node.value)
-
-    def _construct_mapping(loader, node):
-        return YamlDict((loader.construct_object(k), loader.construct_object(v)) for k, v in node.value)
-
-    loader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_SEQUENCE_TAG, _construct_sequence)
-    loader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, _construct_mapping)
+from typing import Type
 
 
 class DynamicYamlLoader(yaml.FullLoader):
-    pass
+    def __init__(self, stream):
+        super().__init__(stream)
+        self.root = None
 
 
-add_constructors(DynamicYamlLoader)
+def add_wrappers(loader: Type[DynamicYamlLoader]):
+    from .yaml_wrappers import YamlDict, YamlList
+
+    def _add_dict_wrapper(loader_: DynamicYamlLoader, node: yaml.MappingNode):
+        return YamlDict(((loader_.construct_object(key), loader_.construct_object(value)) for key, value in node.value))
+
+    def _add_list_wrapper(loader_: DynamicYamlLoader, node):
+        return YamlList((loader_.construct_object(child) for child in node.value))
+
+    loader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, _add_dict_wrapper)
+    loader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_SEQUENCE_TAG, _add_list_wrapper)
+
+
+add_wrappers(DynamicYamlLoader)
 
 
 def load(stream, loader=DynamicYamlLoader):
-    data = yaml.load(stream, Loader=loader)
-    data.set_as_root()
-    return data
+    result = yaml.load(stream, Loader=loader)
+    result.set_as_root()
+    return result
